46a65642c0c6dd36adda005161d123b80aa90eaf
joelmeiller <joel@oscillate.ch>
2021-07-26 08:10:07 +0200

    setup build with typescript

---
+++ src/client/OauthClient.ts
@@ -0,0 +1,259 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+import { AuthenticationResult } from '@azure/msal-browser'
+import {
+  BrowserCacheManager,
+  DEFAULT_BROWSER_CACHE_MANAGER,
+} from '@azure/msal-browser/dist/cache/BrowserCacheManager'
+import {
+  BrowserConfiguration,
+  buildConfiguration,
+} from '@azure/msal-browser/dist/config/Configuration'
+import { CryptoOps } from '@azure/msal-browser/dist/crypto/CryptoOps'
+import { name, version } from '@azure/msal-browser/dist/packageMetadata'
+import { BrowserCacheLocation } from '@azure/msal-browser/dist/utils/BrowserConstants'
+import {
+  AuthenticationScheme,
+  Authority,
+  AuthorityFactory,
+  AuthorityOptions,
+  BaseAuthRequest,
+  DEFAULT_CRYPTO_IMPLEMENTATION,
+  ICrypto,
+  INetworkModule,
+  Logger,
+  ServerTelemetryManager,
+  ServerTelemetryRequest,
+  TimeUtils,
+} from '@azure/msal-common'
+import { ResponseHandler } from '@azure/msal-common/dist/response/ResponseHandler'
+import { ServerAuthorizationTokenResponse } from '@azure/msal-common/dist/response/ServerAuthorizationTokenResponse'
+
+export type OauthTokenResponse = {
+  access_token: string
+  refresh_token: string
+  id_token: string
+}
+
+export type OauthCredentials = {
+  username: string
+  password: string
+  options?: unknown
+}
+
+const MsalConfig = {
+  auth: {
+    clientId: Cypress.env('REACT_APP_CLIENT_ID') || '',
+    authority: `https://login.microsoftonline.com/${Cypress.env(
+      'REACT_APP_TENANT_ID',
+    )}`,
+  },
+  cache: {
+    cacheLocation: BrowserCacheLocation.LocalStorage,
+    storeAuthStateInCookie: true,
+  },
+}
+
+export class OauthClient {
+  // Crypto interface implementation
+  protected readonly browserCrypto: ICrypto
+
+  // Storage interface implementation
+  protected readonly browserStorage: BrowserCacheManager
+
+  // Network interface implementation
+  protected readonly networkClient: INetworkModule
+
+  // Input configuration by developer/user
+  protected config: BrowserConfiguration
+
+  // Logger
+  protected logger: Logger
+
+  // Flag to indicate if in browser environment
+  protected isBrowserEnvironment: boolean
+
+  /**
+   * @constructor
+   * Constructor for the PublicClientApplication used to instantiate the PublicClientApplication object
+   *
+   * Important attributes in the Configuration object for auth are:
+   * - clientID: the application ID of your application. You can obtain one by registering your application with our Application registration portal : https://portal.azure.com/#blade/Microsoft_AAD_IAM/ActiveDirectoryMenuBlade/RegisteredAppsPreview
+   * - authority: the authority URL for your application.
+   * - redirect_uri: the uri of your application registered in the portal.
+   *
+   * In Azure AD, authority is a URL indicating the Azure active directory that MSAL uses to obtain tokens.
+   * It is of the form https://login.microsoftonline.com/{Enter_the_Tenant_Info_Here}
+   * If your application supports Accounts in one organizational directory, replace "Enter_the_Tenant_Info_Here" value with the Tenant Id or Tenant name (for example, contoso.microsoft.com).
+   * If your application supports Accounts in any organizational directory, replace "Enter_the_Tenant_Info_Here" value with organizations.
+   * If your application supports Accounts in any organizational directory and personal Microsoft accounts, replace "Enter_the_Tenant_Info_Here" value with common.
+   * To restrict support to Personal Microsoft accounts only, replace "Enter_the_Tenant_Info_Here" value with consumers.
+   *
+   * In Azure B2C, authority is of the form https://{instance}/tfp/{tenant}/{policyName}/
+   * Full B2C functionality will be available in this library in future versions.
+   *
+   * @param configuration Object for the MSAL PublicClientApplication instance
+   */
+  constructor() {
+    /*
+     * If loaded in an environment where window is not available,
+     * set internal flag to false so that further requests fail.
+     * This is to support server-side rendering environments.
+     */
+    this.isBrowserEnvironment = typeof window !== 'undefined'
+    // Set the configuration.
+    this.config = buildConfiguration(MsalConfig, this.isBrowserEnvironment)
+
+    // Initialize logger
+    this.logger = new Logger(this.config.system.loggerOptions, name, version)
+
+    // Initialize the network module class.
+    this.networkClient = this.config.system.networkClient
+
+    // Initialize redirectResponse Map
+
+    if (!this.isBrowserEnvironment) {
+      this.browserStorage = DEFAULT_BROWSER_CACHE_MANAGER(
+        this.config.auth.clientId,
+        this.logger,
+      )
+      this.browserCrypto = DEFAULT_CRYPTO_IMPLEMENTATION
+      return
+    }
+
+    // Initialize the crypto class.
+    this.browserCrypto = new CryptoOps()
+
+    // Initialize the browser storage class.
+    this.browserStorage = new BrowserCacheManager(
+      this.config.auth.clientId,
+      this.config.cache,
+      this.browserCrypto,
+      this.logger,
+    )
+  }
+
+  async handleToken(
+    request: BaseAuthRequest,
+    response: ServerAuthorizationTokenResponse,
+  ): Promise<AuthenticationResult> {
+    const reqTimestamp = TimeUtils.nowSeconds()
+
+    const responseHandler = new ResponseHandler(
+      this.config.auth.clientId,
+      this.browserStorage,
+      this.browserCrypto,
+      this.logger,
+      null,
+      null,
+    )
+
+    // Validate response. This function throws a server error if an error is returned by the server.
+    responseHandler.validateTokenResponse(response)
+    const discoveredAuthority = await this.getDiscoveredAuthority(
+      request.authority,
+      request.correlationId,
+    )
+
+    return await responseHandler.handleServerTokenResponse(
+      response,
+      discoveredAuthority,
+      reqTimestamp,
+      request,
+    )
+  }
+
+  async setToken(response: ServerAuthorizationTokenResponse) {
+    const baseRequest = this.initializeBaseRequest()    
+    return await this.handleToken(baseRequest, response)
+  }
+
+  /**
+   * Used to get a discovered version of the default authority.
+   * @param requestAuthority
+   * @param requestCorrelationId
+   */
+  async getDiscoveredAuthority(
+    requestAuthority?: string,
+    requestCorrelationId?: string,
+  ): Promise<Authority> {
+    this.logger.verbose('getDiscoveredAuthority called', requestCorrelationId)
+    const authorityOptions: AuthorityOptions = {
+      protocolMode: this.config.auth.protocolMode,
+      knownAuthorities: this.config.auth.knownAuthorities,
+      cloudDiscoveryMetadata: this.config.auth.cloudDiscoveryMetadata,
+      authorityMetadata: this.config.auth.authorityMetadata,
+    }
+
+    if (requestAuthority) {
+      this.logger.verbose(
+        'Creating discovered authority with request authority',
+        requestCorrelationId,
+      )
+      return await AuthorityFactory.createDiscoveredInstance(
+        requestAuthority,
+        this.config.system.networkClient,
+        this.browserStorage,
+        authorityOptions,
+      )
+    }
+
+    this.logger.verbose(
+      'Creating discovered authority with configured authority',
+      requestCorrelationId,
+    )
+    return await AuthorityFactory.createDiscoveredInstance(
+      this.config.auth.authority,
+      this.config.system.networkClient,
+      this.browserStorage,
+      authorityOptions,
+    )
+  }
+
+  /**
+   * Initializer function for all request APIs
+   * @param request
+   */
+  protected initializeBaseRequest(): BaseAuthRequest {
+    const authenticationScheme = AuthenticationScheme.BEARER
+    const authority = this.config.auth.authority
+    const correlationId = this.browserCrypto.createNewGuid()
+    const scopes = []
+
+    const validatedRequest: BaseAuthRequest = {
+      authenticationScheme,
+      correlationId,
+      authority,
+      scopes,
+    }
+
+    return validatedRequest
+  }
+
+  /**
+   *
+   * @param apiId
+   * @param correlationId
+   * @param forceRefresh
+   */
+  protected initializeServerTelemetryManager(
+    apiId: number,
+    correlationId: string,
+    forceRefresh?: boolean,
+  ): ServerTelemetryManager {
+    this.logger.verbose(
+      'initializeServerTelemetryManager called',
+      correlationId,
+    )
+    const telemetryPayload: ServerTelemetryRequest = {
+      clientId: this.config.auth.clientId,
+      correlationId: correlationId,
+      apiId: apiId,
+      forceRefresh: forceRefresh || false,
+    }
+
+    return new ServerTelemetryManager(telemetryPayload, this.browserStorage)
+  }
+}
---
+++ src/index.ts
@@ -0,0 +1,98 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+import { ServerAuthorizationTokenResponse } from '@azure/msal-common/dist/response/ServerAuthorizationTokenResponse'
+import { OauthClient, OauthCredentials } from './client/OauthClient'
+
+export const AzureTokenUrl = `https://login.microsoftonline.com/${Cypress.env(
+  'REACT_APP_TENANT_ID',
+)}/oauth2/v2.0/token`
+
+Cypress.Commands.add(
+  'authCreateAccessToken',
+  function (loginParams: OauthCredentials) {
+    // const loginClient = new UsernamePasswordClientApplication()
+    // return loginClient.acquireTokenByUsernamePassword(credentials)
+    // cy.exec(`az account get-access-token --resource api://04269503-a311-4d14-90ec-d486ff2413f8 --tenant b361b36b-d273-4bad-9010-8a2e74802720 | jq --raw-output '.accessToken'`).then()
+    cy.clearLocalStorage()
+
+    Cypress.log({
+      name: 'loginBySingleSignOn',
+    })
+
+    cy.clearLocalStorage()
+
+    const options = {
+      method: 'POST',
+      url: AzureTokenUrl,
+      qs: {
+        // use qs to set query string to the url that creates
+        // http://auth.corp.com:8080?redirectTo=http://localhost:7074/set_token
+        redirectTo: 'http://localhost:3002',
+      },
+      form: true, // we are submitting a regular form body
+      body: {
+        grant_type: 'password', //read up on the other grant types, they are all useful, client_credentials and authorization_code
+        client_id: Cypress.env('REACT_APP_CLIENT_ID'), //obtained from the application section in AzureAD
+        client_info: 1,
+        // client_secret = {client-secret}//obtained from the application section in AzureAD
+        scope:
+          'api://04269503-a311-4d14-90ec-d486ff2413f8/all user.read openid profile offline_access',
+        username: loginParams.username,
+        password: loginParams.password,
+      },
+    }
+
+    // allow us to override defaults with passed in overrides
+    Cypress._.extend(options, loginParams.options)
+
+    cy.request(options).then((res) => {
+      expect(res.status).to.eq(200)
+      return res.body as ServerAuthorizationTokenResponse
+    })
+  },
+)
+
+const oauthClient = new OauthClient()
+const setToken = (authResponse: ServerAuthorizationTokenResponse) =>
+  new Cypress.Promise((resolve, reject) => {
+    oauthClient.setToken(authResponse).then(resolve).catch(reject)
+  })
+Cypress.Commands.add('authLogin', function (response: unknown) {
+  const authResponse = response as ServerAuthorizationTokenResponse
+
+  cy.intercept('POST', AzureTokenUrl, (req) => {
+    req.continue((res) => {
+      res.headers['Access-Control-Allow-Methods'] = 'POST, OPTIONS'
+      res.headers['Access-Control-Allow-Origin'] = '*'
+    })
+  })
+
+  cy.wrap(null).then(async () => {
+    await setToken(authResponse)
+    cy.visit('/login')
+  })
+})
+
