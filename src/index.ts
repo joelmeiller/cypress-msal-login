8eed53c07b7684a84d4800fcfb2815e9061cf639
joelmeiller <joel@oscillate.ch>
2023-04-18 15:13:17 +0200

    use wrap for async function

--- src/index.ts
+++ src/index.ts
@@ -78,16 +78,13 @@ Cypress.Commands.add(
     // allow us to override defaults with passed in overrides
     Cypress._.extend(options, loginParams.options)
 
-    cy.request(options)
-      .then((res) => {
-        expect(res.status).to.eq(200)
-        const tokenResponse = res.body as ServerAuthorizationTokenResponse
-        cy.wrap(tokenResponse.access_token).as('sessionToken')
-        return tokenResponse
-      })
-      .then(async (tokenResponse) => {
-        await setToken(tokenResponse)
-        cy.reload()
-      })
+    cy.request(options).then((res) => {
+      expect(res.status).to.eq(200)
+      const tokenResponse = res.body as ServerAuthorizationTokenResponse
+      cy.wrap(tokenResponse.access_token).as('sessionToken')
+
+      cy.wrap(async () => await setToken(tokenResponse))
+      cy.reload()
+    })
   },
 )
